f16a15df2549baa7fb508ce8994e05d29cdabb74
Scott Adie <scottynosabe@gmail.com>
2016-04-10 20:49:44 -0400

    implemented flux architecture via alt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,72 +1,47 @@
-import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
-
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
 
 export default  class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: "Learn Webpack"
-        },
-        {
-          id: uuid.v4(),
-          task: "Learn What?"
-        },
-        {
-          id: uuid.v4(),
-          task: "Learn Things"
-        },
-      ]
-    };
-  }
 
   render() {
-    const notes = this.state.notes;
 
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
+
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes notes={notes}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
       );
   }
 
-  deleteNote = (id, e) => {
+  deleteNote(id, e) {
     e.stopPropagation();
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
+    NoteActions.delete(id);
+  }
 
   addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'What is this???'
-      }])
-    });
+    NoteActions.create({task: 'New Task'});
   };
 
-  editNote = (id, task) => {
+  editNote(id, task) {
     if(!task.trim()) {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-
-      return note;
-    });
-
-    this.setState({notes});
-  };
+    NoteActions.update({id, task})
+  }
 }
---
+++ app/libs/alt.js
@@ -0,0 +1,7 @@
+import Alt from 'alt';
+import chromeDebug from 'alt-utils/lib/chromeDebug';
+
+const alt = new Alt();
+Alt.debug('alt', alt);
+
+export default alt;
