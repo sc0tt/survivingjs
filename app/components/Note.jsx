1e7d01389dc40cfdc2f76d4ed53e3cf2b80637f0
Scott Adie <scottynosabe@gmail.com>
2016-04-10 22:56:46 -0400

    Initial drag-dropping

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,72 +1,42 @@
 import React from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
+    };
+  }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(sourceId !== targetId) {
+      targetProps.onMove({sourceId, targetId});
+    }
+
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
 export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      editing: false
-    }
-  }
-
   render() {
-    if(this.state.editing) {
-      return this.renderEdit();
-    }
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
 
-    return this.renderNote();
+    return connectDragSource(connectDropTarget(
+      <li {...this.props}>{this.props.children}</li>
+    ));
   }
-
-  renderEdit = () => {
-    return <input type="text"
-      ref={
-        (e) => e ? e.selectionStart = this.props.task.length : null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-        {onDelete ? this.renderDelete() : null }
-      </div>
-    );
-  };
-
-  renderDelete = () => {
-    return <button
-    className="delete-note"
-    onClick={this.props.onDelete}>x</button>;
-  }
-
-  edit = () => {
-    this.setState({
-      editing: true
-    })
-  }
-
-  checkEnter = (e) => {
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  }
-
-  finishEdit = (e) => {
-    const value = e.target.value;
-
-    if(this.props.onEdit) {
-      this.props.onEdit(value);
-
-      this.setState({
-        editing: false
-      });
-    }
-  };
 }
-
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -67,6 +67,12 @@ class LaneStore {
 
     this.setState({lanes});
   }
+
+  move({sourceId, targetId}) {
+    console.log(`source: ${sourceId}, target: ${targetId}`);
+  }
 }
+
+
 
 export default alt.createStore(LaneStore, 'LaneStore');
